bd78301a77e586b990913a751f43af0d733adb3f
ArnaudK <arnaudkif@gmail.com>
2021-05-31 15:42:07 +0200

    réorganisation du code et ajout de commentaires

--- CountOnMe/Model/CalculModel.swift
+++ CountOnMe/Model/CalculModel.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class CalculModel {
+    let space = " "
 
     var calculText: String = "1 + 1 = 2" {
         didSet {
@@ -16,22 +17,34 @@ class CalculModel {
         }
     } // end of calculText
 
+    /// Transforms calculText into an array of Strings using the spaces between each character to separate them
     var elements : [String] {
         return calculText.split(separator: " ").map { "\($0)" }
     } // end of elements
 
-    let space = " "
+    // MARK: - Error check computed variables
+
+    /// This boolean check that the text does not end with + or - or x or ÷ or =
+    var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" && elements.last != "="
+    } // end of expressionIsCorrect
+
+    /// This boolean check the presence of = and check the number of elements in elements
+    var secondToLastIsEqual: Bool {
+return elements.count > 2 && elements[elements.count - 2] == "="
+    } // end of secondToLastIsNotEqual
 
     func sendAlertNotification(message : String) {
         let alertName = Notification.Name("alertDisplay")
         NotificationCenter.default.post(name: alertName, object: nil, userInfo: ["message": message])
     } // end of sendAlertNotification
 
+    // MARK: - Buttons methods
+
     func numberButtonTaped(butttonTitle: String) {
         if secondToLastIsEqual || elements.last == "=" {
             clearCalculText()
         }
-        print(butttonTitle)
         calculText.append(butttonTitle)
     } // end of numberButtonTaped
 
@@ -40,7 +53,6 @@ class CalculModel {
             lastResultIsCalculText()
         }
         inverseLastNumber()
-        print("+/-")
     } // end of inverseButtonTaped
 
     func operatorButtonTaped(operatorString: String) {
@@ -49,7 +61,6 @@ class CalculModel {
                 lastResultIsCalculText()
             }
             calculText.append("\(space)\(operatorString)\(space)")
-            print(operatorString)
         } else { sendAlertNotification(message: "Impossible de mettre deux opérateurs à la suite ! Veuillez entrer un chiffre.")}
     } // end of operatorButtonTaped
 
@@ -59,15 +70,25 @@ class CalculModel {
                 calculText.append("0")
             }
             calculText.append(".")
-            print(".")
         }
     } // end fo PointButtonTaped
 
     func acButtonTaped() {
-        print("AC")
         removeLastElement()
     } // end of acButtonTaped
 
+    func acButtonMultiTaped() {
+        clearCalculText()
+    } // end of acButtonMultiTaped
+
+    func equalButtonTaped() {
+        if expressionIsCorrect && elements.firstIndex(of: "=") == nil {
+            let result = makeTheCalcul()
+            calculText.append("\(space)=\(space)\(result)")
+        } else { sendAlertNotification(message: "Impossible ! Erreur de synthaxe dans votre opération. Veuillez entrer un chiffre ou corriger avec la touche AC.")}
+    } // end of equalButtonTaped
+
+    /// This function deletes the last element of the array elements
     func removeLastElement() {
         var textCalcul = elements
         guard textCalcul != [] else { return }
@@ -81,28 +102,19 @@ class CalculModel {
         }
     } // end of removeLastElement
 
-    func acButtonMultiTaped() {
-        clearCalculText()
-    } // end of acButtonMultiTaped
-
+    /// This function clear the text
     func clearCalculText() {
         calculText = ""
     } // end of clearCalculText
 
+    /// This function takes the last element of elements and replaces all the text with this one
     func lastResultIsCalculText() {
         let lastResult = elements.last!
         clearCalculText()
         calculText.append("\(lastResult)")
     } // end fo lastResultIsCalculText
 
-    func equalButtonTaped() {
-        if expressionIsCorrect && elements.firstIndex(of: "=") == nil {
-            let result = makeTheCalcul()
-            calculText.append("\(space)=\(space)\(result)")
-            print("=")
-        } else { sendAlertNotification(message: "Impossible ! Erreur de synthaxe dans votre opération. Veuillez entrer un chiffre ou corriger avec la touche AC.")}
-    } // end of equalButtonTaped
-
+    /// This function reverses the last number entered by adding - or deleting
     func inverseLastNumber() {
         var textCalcul = elements
         guard textCalcul != [] else { return }
@@ -122,24 +134,14 @@ class CalculModel {
         }
     } // end of inverseLastNumber
 
-    // MARK: - Error check computed variables
-    /// vérifie que le texte ne finit pas par + ni - ni  * ni /
-    var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" && elements.last != "="
-    } // end of expressionIsCorrect
-
-    var secondToLastIsEqual: Bool {
-return elements.count > 2 && elements[elements.count - 2] == "="
-    } // end of secondToLastIsNotEqual
-
     // MARK: - Calcul Methods
 
+    /// This function calculates the result of the operation
     func makeTheCalcul() -> String {
         var operationsToReduce = elements
         var priorityOperator: Bool {
             return (operationsToReduce.firstIndex(of: "x") != nil) || (operationsToReduce.firstIndex(of: "÷") != nil)
         }
-
         while operationsToReduce.count > 1 {
             while priorityOperator {
                 if let indexTempOfOperator = operationsToReduce.firstIndex(where: {$0 == "x" || $0 == "÷"}) {
@@ -178,6 +180,7 @@ return elements.count > 2 && elements[elements.count - 2] == "="
         return operationsToReduce.first!
     } // end of makeTheCalcul
 
+    /// This function formats a Double into a String with or without the comma as required
     func doubleToInteger(currentResult: Double) -> String {
         let doubleAsString = NumberFormatter.localizedString(from: (NSNumber(value: currentResult)), number: .decimal)
         return doubleAsString
